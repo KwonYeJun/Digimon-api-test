43d1f8b6560f5d2545b13a86972350913f9d093d
gwonyejun <kyjuni1059@gmail.com>
2023-03-21 17:57:23 +0900

    디지몬 api 수정 해야 합니다

--- app.js
+++ app.js
@@ -5,26 +5,26 @@ let listtest = [];
 
 function test(i) {
   const xhr = new XMLHttpRequest();
- 
+
   const _URL = `https://www.digi-api.com/api/v1/digimon?page=${i}`;
   xhr.open('GET', _URL);
   xhr.send();
   xhr.addEventListener('load', function () {
 
     const Digimon = JSON.parse(xhr.response);
-    
 
-  //   for(let i=0;i <Digimon.content.length;i++){
-  //   createimg.setAttribute('src',Digimon.content[0].image)
-  // }
+
+    //   for(let i=0;i <Digimon.content.length;i++){
+    //   createimg.setAttribute('src',Digimon.content[0].image)
+    // }
     // console.dir(Digimon.content[0].image);
     let vvv = Digimon.content[0].image;
     console.dir(vvv);
     let createimg = document.createElement('img');
-    createimg.setAttribute("src",vvv);
-    root.appendChild(createimg); 
-return vvv;
-    
+    createimg.setAttribute("src", vvv);
+    root.appendChild(createimg);
+    return vvv;
+
   })
 }
 
